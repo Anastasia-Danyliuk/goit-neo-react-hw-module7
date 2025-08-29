95353cf09aafda416c0cade93dee51ab2f52b063
bexots64 <bexots64@gmail.com>
2025-08-29 21:21:31 +0300

    Add react hw7

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,20 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import storage from "redux-persist/lib/storage";
-import { persistStore, persistReducer } from "redux-persist";
 import { contactsReducer } from "./contactsSlice";
 import { filtersReducer } from "./filtersSlice";
 
-const contactsPersistConfig = {
-    key: "contacts",
-    storage,
-    whitelist: ["items"],
-};
-
 export const store = configureStore({
     reducer: {
-        contacts: persistReducer(contactsPersistConfig, contactsReducer),
+        contacts: contactsReducer,
         filters: filtersReducer,
     },
 });
-
-export const persistor = persistStore(store);
